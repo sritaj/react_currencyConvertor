a5b00d7f4e027f50e68e130826d2127a508920f4
Patel Sritaj <patelsritaj@outlook.com>
2024-05-16 00:51:34 +0530

    Added missing configuration for tailwind react and dummy code

---
+++ src/CurrencyConvertor.jsx
@@ -0,0 +1,13 @@
+const CurrencyConvertor = () => {
+  return (
+    <div className="container max-w-auto flex justify-center pt-6">
+      <div>
+        <h1 className="text-center font-mono text-xl text-gray-900">
+          Currency Convertor
+        </h1>
+      </div>
+    </div>
+  );
+};
+
+export default CurrencyConvertor;
