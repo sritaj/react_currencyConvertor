e29f0c4676b7cecb481cbf499083aa46241d6558
Patel Sritaj <patelsritaj@outlook.com>
2024-05-16 21:26:54 +0530

    Added button with Tailwind styling

--- src/CurrencyConvertor.jsx
+++ src/CurrencyConvertor.jsx
@@ -12,22 +12,27 @@ const CurrencyConvertor = () => {
           ></select>
           <input
             type="number"
-            id="number-input"
+            id="number-input-1"
             className="ml-4 px-2 bg-gray-50 border-gray-300 outline outline-offset2 outline-cyan-500/50 rounded"
             placeholder="0"
           ></input>
         </div>
         <div className="mt-6">
           <select
-            id="currency-1"
+            id="currency-2"
             className=" bg-gray-50 border-gray-300 outline outline-offset2 outline-amber-500/50 rounded w-16"
           ></select>
           <input
             type="number"
-            id="number-input"
+            id="number-input-2"
             className="ml-4 px-2 bg-gray-50 border-gray-300 outline outline-offset2 outline-amber-500/50 rounded"
             placeholder="0"
           ></input>
+        </div>
+        <div className="mt-6 flex justify-center">
+          <button className="text-white bg-blue-700 hover:bg-blue:800 rounded-lg px-5 py-1">
+            Convert
+          </button>
         </div>
       </div>
     </div>
