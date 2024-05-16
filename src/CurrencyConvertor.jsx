c0c32b69809e58aedf3f088694861c684b087005
Patel Sritaj <patelsritaj@outlook.com>
2024-05-16 21:00:42 +0530

    Added number text box with tailwing styling

--- src/CurrencyConvertor.jsx
+++ src/CurrencyConvertor.jsx
@@ -5,6 +5,15 @@ const CurrencyConvertor = () => {
         <h1 className="text-center mt-6 text-cyan-700 font-serif">
           Currency Convertor
         </h1>
+        <div className="mt-6">
+          <select></select>
+          <input
+            type="number"
+            id="number-input"
+            className="ml-4 px-2 bg-gray-50 border-gray-300 outline outline-offset2 outline-cyan-500/50 rounded"
+            placeholder="0"
+          ></input>
+        </div>
       </div>
     </div>
   );
