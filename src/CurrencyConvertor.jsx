ca7250f0fed1b6e9f95e841a018393aad248eb61
Patel Sritaj <patelsritaj@outlook.com>
2024-05-21 18:31:33 +0530

    Added useState for the required four fields and updated IDs

--- src/CurrencyConvertor.jsx
+++ src/CurrencyConvertor.jsx
@@ -1,4 +1,11 @@
+import { useState } from "react";
+
 const CurrencyConvertor = () => {
+  const [fromAmount, setFromAmount] = useState(0);
+  const [toAmount, setToAmount] = useState(0);
+  const [fromCurrencyDropdown, setFromCurrencyDropdown] = useState("INR");
+  const [toCurrencyDropdown, setToCurrencyDropdown] = useState("DOLLAR");
+
   return (
     <div className="w-full flex justify-center">
       <div>
@@ -7,30 +14,33 @@ const CurrencyConvertor = () => {
         </h1>
         <div className="mt-6">
           <select
-            id="currency-1"
+            id="from-currency"
             className=" bg-gray-50 border-gray-300 outline outline-offset2 outline-cyan-500/50 rounded w-16"
           ></select>
           <input
             type="number"
-            id="number-input-1"
+            id="from-amount"
             className="ml-4 px-2 bg-gray-50 border-gray-300 outline outline-offset2 outline-cyan-500/50 rounded"
             placeholder="0"
           ></input>
         </div>
         <div className="mt-6">
           <select
-            id="currency-2"
+            id="to-currency"
             className=" bg-gray-50 border-gray-300 outline outline-offset2 outline-amber-500/50 rounded w-16"
           ></select>
           <input
             type="number"
-            id="number-input-2"
+            id="to-amount"
             className="ml-4 px-2 bg-gray-50 border-gray-300 outline outline-offset2 outline-amber-500/50 rounded"
             placeholder="0"
           ></input>
         </div>
         <div className="mt-6 flex justify-center">
-          <button className="text-white bg-blue-700 hover:bg-blue:800 rounded-lg px-5 py-1">
+          <button
+            id="convert"
+            className="text-white bg-blue-700 hover:bg-blue:800 rounded-full px-5 py-1"
+          >
             Convert
           </button>
         </div>
