fddd459ffb3d76495d0e5a40efb900886d552992
Patel Sritaj <patelsritaj@outlook.com>
2024-05-23 16:37:45 +0530

    Added new component Swap and updated Currency Convertor

--- src/components/CurrencyConvertor.jsx
+++ src/components/CurrencyConvertor.jsx
@@ -2,6 +2,7 @@ import { useState, useEffect } from "react";
 import axios from "axios";
 import SelectDropdown from "./SelectDropdown";
 import ExchangeMessage from "./ExchangeMessage";
+import Swap from "./Swap";
 
 const CurrencyConvertor = () => {
   const [fromAmount, setFromAmount] = useState(0);
@@ -81,28 +82,7 @@ const CurrencyConvertor = () => {
             currencyOptions={currencyOptions}
           />
 
-          <div className="relative inline-flex items-center justify-center w-full">
-            <hr className="w-full h-px my-8 bg-amber-600 border-0"></hr>
-            <div className="absolute px-4 -translate-x-1/2  left-1/2">
-              <button
-                className="text-white bg-orange-500 hover:bg-orange-700 rounded-full px-2 py-1 ring-offset-1 ring-1 ring-orange-500 ring-offset-orange-400 align-middle"
-                onClick={swapFields}
-              >
-                <svg
-                  className="w-6 h-6 mr-0 fill-current stroke-current"
-                  xmlns="http://www.w3.org/2000/svg"
-                  viewBox="0 0 24 24"
-                  strokeWidth="2"
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                >
-                  <path d="M8 7l4-4 4 4M12 3v18" />
-
-                  <path d="M16 17l-4 4-4-4M12 21V3" />
-                </svg>
-              </button>
-            </div>
-          </div>
+          <Swap swapFields={swapFields} />
 
           <SelectDropdown
             amountDefault={toAmount}
